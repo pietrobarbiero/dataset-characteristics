9366ef1533d908bc9afe1c352f4597335a4b39c8
Pietro Barbiero <barbiero@tutanota.com>
2021-09-17 14:51:08 +0100

    Computing dataset stats

--- postprocessing.py
+++ postprocessing.py
@@ -1,13 +1,21 @@
 """
 Script to perform post-processing of the results.
 """
+import math
 import os
 import numpy as np
+import openml
 import pandas as pd
 import re
 import sys
 
+from sklearn.impute import KNNImputer
 from sklearn.metrics import accuracy_score, f1_score, matthews_corrcoef
+from sklearn.preprocessing import StandardScaler
+
+from convex_hull_stats import homogeneity_class_covariances, feature_correlation_class, normality_departure, \
+    information, dimensionality_stats, class_stats
+
 
 def is_experiment_complete(dataset_folder) :
     """
@@ -84,63 +92,174 @@ def main() :
     ml_algorithm_names = sorted(list(set(ml_algorithm_names)))
     print("Here is the list of all ML algorithms appearing at least once in the folders:", ml_algorithm_names)
 
-    # right now it's not really post-processing, it's just a comparison between the different classifiers
-    for dataset_folder in dataset_folders :
-        dataset_name = os.path.basename(dataset_folder)
-        print("Now analyzing folder for dataset \"%s\"..." % dataset_name)
+    dataset_names = [os.path.basename(dataset_folder) for dataset_folder in dataset_folders]
 
-        # setup global data structure
-        if "dataset" not in stats : stats["dataset"] = []
-        stats["dataset"].append(dataset_name)
+    print("Loading benchmark suite OpenML-CC18...")
+    benchmark_suite = openml.study.get_suite('OpenML-CC18')
 
-        # get the list of cross-validation experiments
-        cv_folders = [ f.path for f in os.scandir(dataset_folder) if f.is_dir() ]
+    # list of datasets in which we have issues
+    datasets_with_issues = ["mnist_784", "Bioresponse"]
 
-        for cv_folder in cv_folders :
-            print("Now analyzing folder for \"%s\" for dataset \"%s\"..." % (os.path.basename(cv_folder), dataset_name))
+    for task_id in benchmark_suite.tasks:
 
-            # prepare local data structure
-            performance = dict()
+        task = openml.tasks.get_task(task_id)
+        dataset = task.get_dataset()
+        if dataset.name in dataset_names :
+            dataset_name = dataset.name
+            dataset_folder = os.path.join('results', dataset_name)
 
-            # let's start collecting information from each fold
-            fold_folders = [ f.path for f in os.scandir(cv_folder) if f.is_dir() ]
+            # get data
+            X, y = task.get_X_and_y()
 
-            for fold_folder in fold_folders :
-                fold_number = int(re.search("([0-9]+)", os.path.basename(fold_folder)).group(1))
+            if np.any(np.isnan(X).flatten()):
+                print("Missing samples in task, calling imputer...")
+                imputer = KNNImputer()
+                X = imputer.fit_transform(X)
 
-                # read some information, to be used later
-                df_test = pd.read_csv(os.path.join(fold_folder, "y_test.csv"))
-                y_test = df_test["class_label"].values
+            print("Now analyzing folder for dataset \"%s\"..." % dataset_name)
 
-                # get the list of folders (here representing different classifiers)
-                classifier_folders = [ f.path for f in os.scandir(fold_folder) if f.is_dir() ]
-                print("Found %d classifiers for fold %d: \"%s\"" % (len(classifier_folders), fold_number, str(classifier_folders))) 
+            # setup global data structure
+            if "dataset" not in stats : stats["dataset"] = []
+            stats["dataset"].append(dataset_name)
 
-                for classifier_folder in classifier_folders :
-                    # get classifier name
-                    classifier_name = os.path.basename(classifier_folder)
-                    
-                    # get statistics (in this case, accuracy on test)
-                    df_pred_test = pd.read_csv(os.path.join(classifier_folder, "y_test_pred.csv"))
-                    y_pred_test = df_pred_test["class_label"].values
+            # get the list of cross-validation experiments
+            cv_folders = [ f.path for f in os.scandir(dataset_folder) if f.is_dir() ]
 
-                    # create local dictionary of metrics, using a list of function pointers!
-                    # update: it works poorly, because some metrics need special values for their arguments in specific cases
-                    metrics_dict = dict()
-                    metrics_dict[accuracy_score.__name__] = accuracy_score(y_test, y_pred_test)
-                    metrics_dict[matthews_corrcoef.__name__] = matthews_corrcoef(y_test, y_pred_test)
+            for cv_folder in cv_folders :
+                print("Now analyzing folder for \"%s\" for dataset \"%s\"..." % (os.path.basename(cv_folder), dataset_name))
 
-                    if len(np.unique(y_test)) == 2 :
-                        metrics_dict[f1_score.__name__] = f1_score(y_test, y_pred_test)
-                    else :
-                        metrics_dict[f1_score.__name__] = f1_score(y_test, y_pred_test, average='weighted')
+                # prepare local data structure
+                performance = dict()
+                datasetstats = dict()
 
-                    # store performance, as a dictionary (classifier) of dictionaries (metrics) of lists (performance per fold)
-                    if classifier_name not in performance : performance[classifier_name] = dict()
-                    for metric_name, metric_performance in metrics_dict.items() :
-                        if metric_name not in performance[classifier_name] : performance[classifier_name][metric_name] = []
-                        performance[classifier_name][metric_name].append(metric_performance) 
-            
+                dataset_metrics_name = ['levene_stat', 'levene_pvalue', 'levene_success', 'feature_avg_correlation',
+                                   'feature_avg_skew', 'feature_avg_kurtosis', 'feature_avg_mutual_information',
+                                   'dimensionality', 'intrinsic_dimensionality', 'intrinsic_dimensionality_ratio',
+                                   'feature_noise', 'distances', 'sample_avg_distance', 'sample_std_distance',
+                                   'imbalance_ratio_in_hull', 'imbalance_ratio_out_hull', 'imbalance_ratio_train',
+                                   'imbalance_ratio_val', 'in_hull_ratio', 'out_hull_ratio']
+
+                # let's start collecting information from each fold
+                fold_folders = [ f.path for f in os.scandir(cv_folder) if f.is_dir() ]
+
+                for fold_folder in fold_folders :
+                    fold_number = int(re.search("([0-9]+)", os.path.basename(fold_folder)).group(1))
+
+                    # read some information, to be used later
+                    df_train = pd.read_csv(os.path.join(fold_folder, "y_train.csv"))
+                    y_train = df_train["class_label"].values
+                    df_test = pd.read_csv(os.path.join(fold_folder, "y_test.csv"))
+                    y_test = df_test["class_label"].values
+                    train_idx = pd.read_csv(os.path.join(fold_folder, "train_indexes.csv")).values.squeeze()
+                    test_idx = pd.read_csv(os.path.join(fold_folder, "test_indexes.csv")).values.squeeze()
+                    in_indexes = pd.read_csv(os.path.join(fold_folder, "in_indexes.csv")).values.squeeze()
+                    out_indexes = pd.read_csv(os.path.join(fold_folder, "out_indexes.csv")).values.squeeze()
+
+                    X_train = X[train_idx]
+                    X_test = X[test_idx]
+
+                    # data preprocessing
+                    scaler = StandardScaler()
+                    ss = scaler.fit(X_train)
+                    X_train_scaled = ss.transform(X_train)
+                    X_test_scaled = ss.transform(X_test)
+
+                    # compute training set stats
+                    print("Split: %d - Computing data set stats..." % fold_number)
+                    levene_stat, levene_pvalue, levene_success = homogeneity_class_covariances(X_train_scaled, y_train)
+                    if math.isnan(levene_pvalue):
+                        levene_pvalue = -1
+                    if math.isnan(levene_stat):
+                        levene_stat = -1
+                    feature_avg_correlation = feature_correlation_class(X_train_scaled, y_train)
+                    feature_avg_skew, feature_avg_kurtosis = normality_departure(X_train_scaled, y_train)
+                    feature_avg_mutual_information = information(X_train_scaled, y_train)
+                    dimensionality, intrinsic_dimensionality, intrinsic_dimensionality_ratio, feature_noise, distances = dimensionality_stats(X_train_scaled)
+                    sample_avg_distance = np.average(distances, weights=distances)
+                    sample_std_distance = np.std(distances)
+                    print("Split: %d - Data set stats computed!" % fold_number)
+
+                    # convex hull test
+                    print("Split: %d - Computing convex hull..." % fold_number)
+                    imbalance_ratio_in_hull = -1
+                    imbalance_ratio_out_hull = -1
+                    y_in_hull = y_test[in_indexes]
+                    y_out_hull = y_test[out_indexes]
+                    imbalance_ratio_train = class_stats(y_train)
+                    imbalance_ratio_val = class_stats(y_test)
+                    if len(y_in_hull) > 0:
+                        imbalance_ratio_in_hull = class_stats(y_in_hull)
+                    if len(y_out_hull) > 0:
+                        imbalance_ratio_out_hull = class_stats(y_out_hull)
+                    in_hull_ratio = sum(in_indexes) / y_test.shape[0]
+                    out_hull_ratio = sum(out_indexes) / y_test.shape[0]
+                    print("Split: %d - Convex hull computed!" % fold_number)
+
+                    dataset_metrics = [levene_stat, levene_pvalue, levene_success, feature_avg_correlation,
+                                            feature_avg_skew, feature_avg_kurtosis, feature_avg_mutual_information,
+                                            dimensionality, intrinsic_dimensionality, intrinsic_dimensionality_ratio,
+                                            feature_noise, distances, sample_avg_distance, sample_std_distance,
+                                            imbalance_ratio_in_hull, imbalance_ratio_out_hull, imbalance_ratio_train,
+                                            imbalance_ratio_val, in_hull_ratio, out_hull_ratio]
+
+                    if dataset_name not in datasetstats: datasetstats[dataset_name] = dict()
+                    for dataset_metric_name, dataset_metric in zip(dataset_metrics_name, dataset_metrics):
+                        if dataset_metric_name not in datasetstats[dataset_name]: datasetstats[dataset_name][dataset_metric_name] = []
+                        datasetstats[dataset_name][dataset_metric_name].append(dataset_metric)
+
+                    # get the list of folders (here representing different classifiers)
+                    classifier_folders = [ f.path for f in os.scandir(fold_folder) if f.is_dir() ]
+                    print("Found %d classifiers for fold %d: \"%s\"" % (len(classifier_folders), fold_number, str(classifier_folders)))
+
+                    for classifier_folder in classifier_folders :
+                        # get classifier name
+                        classifier_name = os.path.basename(classifier_folder)
+
+                        # get statistics (in this case, accuracy on test)
+                        df_pred_train = pd.read_csv(os.path.join(classifier_folder, "y_train_pred.csv"))
+                        y_pred_train = df_pred_train["class_label"].values
+                        df_pred_test = pd.read_csv(os.path.join(classifier_folder, "y_test_pred.csv"))
+                        y_pred_test = df_pred_test["class_label"].values
+
+                        y_in_hull = y_test[in_indexes]
+                        y_out_hull = y_test[out_indexes]
+                        y_pred_in_hull = y_pred_test[in_indexes]
+                        y_pred_out_hull = y_pred_test[out_indexes]
+
+                        # scores
+                        # create local dictionary of metrics, using a list of function pointers!
+                        # update: it works poorly, because some metrics need special values for their arguments in specific cases
+                        print("Split: %d - Computing predictions..." % fold_number)
+                        test_accuracy_in_hull, test_f1_in_hull, test_mc_in_hull, test_accuracy_out_hull, test_f1_out_hull, test_mc_out_hull = np.nan, np.nan, np.nan, np.nan, np.nan, np.nan
+                        if len(y_in_hull) > 0:
+                            test_accuracy_in_hull = accuracy_score(y_in_hull, y_pred_in_hull)
+                            test_f1_in_hull = f1_score(y_in_hull, y_pred_in_hull, average="weighted")
+                            test_mc_in_hull = matthews_corrcoef(y_in_hull, y_pred_in_hull)
+                        if len(y_out_hull) > 0:
+                            test_accuracy_out_hull = accuracy_score(y_out_hull, y_pred_out_hull)
+                            test_f1_out_hull = f1_score(y_out_hull, y_pred_out_hull, average="weighted")
+                            test_mc_out_hull = matthews_corrcoef(y_out_hull, y_pred_out_hull)
+                        metrics_dict = dict()
+                        metrics_dict[accuracy_score.__name__] = accuracy_score(y_test, y_pred_test)
+                        metrics_dict[matthews_corrcoef.__name__] = matthews_corrcoef(y_test, y_pred_test)
+                        if len(np.unique(y_test)) == 2 :
+                            metrics_dict[f1_score.__name__] = f1_score(y_test, y_pred_test)
+                        else :
+                            metrics_dict[f1_score.__name__] = f1_score(y_test, y_pred_test, average='weighted')
+                        metrics_dict[accuracy_score.__name__ + '_in_hull'] = test_accuracy_in_hull
+                        metrics_dict[f1_score.__name__ + '_in_hull'] = test_f1_in_hull
+                        metrics_dict[matthews_corrcoef.__name__ + '_in_hull'] = test_mc_in_hull
+                        metrics_dict[accuracy_score.__name__ + '_out_hull'] = test_accuracy_out_hull
+                        metrics_dict[f1_score.__name__ + '_out_hull'] = test_f1_out_hull
+                        metrics_dict[matthews_corrcoef.__name__ + '_out_hull'] = test_mc_out_hull
+                        print("Split: %d - Predictions computed!" % fold_number)
+
+                        # store performance, as a dictionary (classifier) of dictionaries (metrics) of lists (performance per fold)
+                        if classifier_name not in performance : performance[classifier_name] = dict()
+                        for metric_name, metric_performance in metrics_dict.items() :
+                            if metric_name not in performance[classifier_name] : performance[classifier_name][metric_name] = []
+                            performance[classifier_name][metric_name].append(metric_performance)
+
         # once we are at this point, computation on all cv folders for the dataset is over, so let's draw some conclusions
         keys_found = []
         for classifier_name, classifier_metrics in performance.items() :
@@ -159,6 +278,25 @@ def main() :
 
                 # record that we had stats for this particular combination of metric and classifier
                 keys_found.append(metric_name + " " + classifier_name)
+
+        # once we are at this point, computation on all cv folders for the dataset is over, so let's draw some conclusions
+        keys_found = []
+        for dataset_name, dataset_metrics in datasetstats.items() :
+            for metric_name, metric_performance in datasetstats.items() :
+                c_mean = np.mean(metric_performance)
+                c_std = np.std(metric_performance)
+                print("Dataset \"%s\", metric \"%s\": mean=%.4f; std=%.4f" % (dataset_name, metric_name, c_mean, c_std))
+
+                # and save everything to the dictionary structure, to be later converted to dataframe
+                key_name_mean = metric_name + " (mean)"
+                key_name_std = metric_name + " (std)"
+                if key_name_mean not in stats : stats[key_name_mean] = []
+                if key_name_std not in stats : stats[key_name_std] = []
+                stats[key_name_mean].append(c_mean)
+                stats[key_name_std].append(c_std)
+
+                # record that we had stats for this particular combination of metric and classifier
+                keys_found.append(metric_name)
 
         # here, we must check whether there are all results for all metrics and all classifiers; if that is
         # not the case, we add other 'None' values for everything in the lists
